d4a45a69faf4fb3aca15ea6701ea68ad980ea624
Farouk Sabiou <sfarouk298@gmail.com>
2020-12-26 10:45:48 +0100

    updates candidates.json
    - add som candidates avatar

--- app/src/main/java/dev/farouk/takara/ui/adapters/BindingAdapters.kt
+++ app/src/main/java/dev/farouk/takara/ui/adapters/BindingAdapters.kt
@@ -4,6 +4,7 @@ import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import coil.load
 import coil.transform.CircleCropTransformation
+import dev.farouk.takara.R
 
 /**
  * Created by Farouk on 19/12/2020.
@@ -13,6 +14,10 @@ fun bindImageFromUrl(view: ImageView, imageUrl: String?) {
     if (!imageUrl.isNullOrEmpty()) {
         view.load(imageUrl) {
             crossfade(true)
+            transformations(CircleCropTransformation())
+        }
+    } else {
+        view.load(R.drawable.ic_baseline_account_circle_24) {
             transformations(CircleCropTransformation())
         }
     }
